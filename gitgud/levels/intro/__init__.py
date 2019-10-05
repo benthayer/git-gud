28c48fda149975e0c559e635df1dfba72f717012
Fiss-and-Fuse <noahb314@gmail.com>
2019-10-05 14:40:30 -0500

    added extra merge level

--- gitgud/levels/intro/__init__.py
+++ gitgud/levels/intro/__init__.py
@@ -6,6 +6,6 @@ from gitgud.levels.util import BasicChallenge
 
 all_challenges = OrderedDict()
 all_challenges['commits'] = BasicChallenge('commits', pkg_resources.resource_filename(__name__, '_commits/'))
-
+all_challenges['merging'] = BasicChallenge('merging', pkg_resources.resource_filename(__name__, '_merging/'))
 del pkg_resources
 del BasicChallenge
