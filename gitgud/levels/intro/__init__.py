c06698ea44a98191b30a509b9a37a8b864c3fcf1
Ben Thayer <ben@benthayer.com>
2019-10-06 16:19:44 -0500

    Fixed error from PR #7

--- gitgud/levels/intro/__init__.py
+++ gitgud/levels/intro/__init__.py
@@ -6,7 +6,7 @@ from gitgud.levels.util import BasicChallenge
 
 all_challenges = OrderedDict()
 all_challenges['commits'] = BasicChallenge('commits', pkg_resources.resource_filename(__name__, '_commits/'))
-all_challenges['branching'] = BasicChallenge('branching', pkg_resources.resource(__name__, '_branching/'))
+all_challenges['branching'] = BasicChallenge('branching', pkg_resources.resource_filename(__name__, '_branching/'))
 
 del pkg_resources
 del BasicChallenge
