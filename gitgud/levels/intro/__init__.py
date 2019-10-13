0163af5b43a3d73a96ce8356cc34bcc3bc07d007
Michael Park <parkmichael777@gmail.com>
2019-10-12 21:03:44 -0500

    renamed and added to init.py

--- gitgud/levels/intro/__init__.py
+++ gitgud/levels/intro/__init__.py
@@ -6,7 +6,8 @@ from gitgud.levels.util import BasicChallenge
 
 all_challenges = OrderedDict()
 all_challenges['commits'] = BasicChallenge('commits', pkg_resources.resource_filename(__name__, '_commits/'))
-all_challenges['branching'] = BasicChallenge('branching', pkg_resources.resource(__name__, '_branching/'))
+all_challenges['branching'] = BasicChallenge('branching', pkg_resources.resource_filename(__name__, '_branching/'))
+all_challenges['rebasing'] = BasicChallenge('rebasing', pkg_resources.resource_filename(__name__, '_rebasing/'))
 
 del pkg_resources
 del BasicChallenge
