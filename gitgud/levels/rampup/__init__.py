80166d35efd991fe20a6242dc5387a8229902721
Michael Park <parkmichael777@gmail.com>
2019-10-29 16:07:13 -0500

    Removed all challenges except relrefs2

--- gitgud/levels/rampup/__init__.py
+++ gitgud/levels/rampup/__init__.py
@@ -5,7 +5,4 @@ import pkg_resources
 from gitgud.levels.util import BasicChallenge
 
 all_challenges = OrderedDict()
-all_challenges['detaching'] = BasicChallenge('detaching', pkg_resources.resource_filename(__name__, '_detaching/'))
-all_challenges['relrefs1'] = BasicChallenge('relrefs1', pkg_resources.resource_filename(__name__, '_relrefs1/'))
 all_challenges['relrefs2'] = BasicChallenge('relrefs2', pkg_resources.resource_filename(__name__, '_relrefs2/'))
-all_challenges['reversing'] = BasicChallenge('reversing', pkg_resources.resource_filename(__name__, '_reversing/'))
