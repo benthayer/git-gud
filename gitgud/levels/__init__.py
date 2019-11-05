b9f39421218607fa87927368439eedf80f3d35c9
Sahan Kumarasinghe <sahansk2@illinois.edu>
2019-11-05 16:01:47 -0600

    all_levels method to constructor, tab size fixes

--- gitgud/levels/__init__.py
+++ gitgud/levels/__init__.py
@@ -4,8 +4,6 @@ from gitgud.levels.merging import level as merging_level
 from gitgud.levels.util import AllLevels
 
 all_levels = AllLevels([
-        intro_level,
-        merging_level
+    intro_level,
+    merging_level
 ])
-
-all_levels._add_next_challenges()
