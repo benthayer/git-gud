ef3b1a81316d4ac14174842d58335f129195ece1
Michael Park <parkmichael777@gmail.com>
2019-11-08 10:40:45 -0600

    Added confirmation, and more file addition stuff

--- make_level.py
+++ make_level.py
@@ -1,39 +1,61 @@
 import sys
 import os
 
-# Obtain input arguments
-skill_name = sys.argv[1]
-level_name = sys.argv[2]
-
-cwd = os.getcwd()
-
-if cwd[-7:] == "git-gud":
-    skill_path = os.path.join("gitgud","skills","{}".format(skill_name))
-    if not os.path.exists(skill_path):
-        os.mkdir(skill_path)
-
-    level_path = os.path.join(skill_path,"_{}".format(level_name))
-    if not os.path.exists(level_path):
-        os.mkdir(level_path)
-
-    inst_path = os.path.join(level_path,"instructions.txt")
-    if not os.path.exists(inst_path):
-        open(inst_path,'a').close()
-
-    goal_path = os.path.join(level_path,"goal.txt")
-    if not os.path.exists(goal_path):
-        open(goal_path,'a').close()
-
-    setup_path = os.path.join(level_path,"setup.spec")
-    if not os.path.exists(setup_path):
-        open(setup_path,'a').close()
-
-    test_path = os.path.join(level_path,"test.spec")
-    if not os.path.exists(test_path):
-        open(test_path,'a').close()
-else:
-    print("Error: Execute this script in the git-gud directory.")
 
 
+def main():
+    # Obtain input arguments
+    skill_name = sys.argv[1]
+    level_name = sys.argv[2]
+    
+    path = os.path
+    
+    # Check if cwd is git-gud folder
+    if cwd[-7:] == "git-gud":
+            # Confirm choice to avoid making a mess
+            print ("Confirm[y/n]: skill_name = \"{}\" & level_name = \"{}\"".format(skill_name,level_name))
+            choice = input().lower()
+            if choice == 'n':
+                return
+            
+            # Make skill folder
+            init_path = path.join("gitgud","skills","{}".format(skill_name))
+            if not path.exists(skill_path):
+                os.mkdir(skill_path)
+    
+            inst_path = path.join(level_path,"instructions.txt")
+            if not path.exists(inst_path):
+                open(inst_path,'a').close()
+    
+            # Make level folder
+            level_path = path.join(skill_path,"_{}".format(level_name))
+            if not path.exists(level_path):
+                os.mkdir(level_path)
+            
+            # Make instruction file
+            inst_path = path.join(level_path,"instructions.txt")
+            if not path.exists(inst_path):
+                open(inst_path,'a').close()
+            
+            # Make goal file
+            goal_path = path.join(level_path,"goal.txt")
+            if not path.exists(goal_path):
+                open(goal_path,'a').close()
+            
+            # Make setup file
+            setup_path = path.join(level_path,"setup.spec")
+            if not path.exists(setup_path):
+                open(setup_path,'a').close()
+    
+            # Make test file
+            test_path = path.join(level_path,"test.spec")
+            if not path.exists(test_path):
+                open(test_path,'a').close()
+    else:
+        print("Error: Execute this script in the git-gud directory.")
+
+
+if __name__ == "__main__":
+    main()
 
 
