bd7f4f3f9c50b79a12c822dea24db17a392f81c3
Michael Park <parkmichael777@gmail.com>
2019-11-08 14:35:12 -0600

    Fixed init writing; script finished

--- make_level.py
+++ make_level.py
@@ -12,6 +12,25 @@ def writeInit(skill_name, skill_path, level_name):
             level_setup = "import pkg_resources\nfrom gitgud.skills.util import BasicLevel\nfrom gitgud.skills.util import Skill\n\nskill = Skill(\n\t'{}',\n\t[\n\t]\n)".format(skill_name)
             fp.write(level_setup)
         fp.close()
+        
+        # Read file
+        with open(join(join("gitgud","skills"),"__init__.py"), 'r') as fp:
+            filedata = fp.read()
+        fp.close()
+
+        # Add import statement
+        replace = "from gitgud.skills.{0} import skill as {0}_skill\n\nfrom gitgud.skills.u".format(skill_name)
+        filedata = filedata.replace("\nfrom gitgud.skills.u", replace)
+        
+        # Add to input array of Skill
+        replace = ",\n\t{}_skill\n]".format(skill_name)
+        filedata = filedata.replace("\n]", replace)
+          
+        # Write to file
+        with open(join(join("gitgud","skills"),"__init__.py"), 'w') as fp:
+            fp.write(filedata)
+        fp.close()
+        
         writeInit(skill_name, skill_path, level_name)
         print(fp.closed) #for debug, delete
         return
@@ -21,26 +40,14 @@ def writeInit(skill_name, skill_path, level_name):
             filedata = fp.read()
         fp.close()
 
-        replace = "\b\b,\n\t\tBasicLevel('{0}', pkg_resources.resource_filename(__name__, '_{0}/'))\n\t\t]".format(level_name)
-        filedata = filedata.replace("]", replace)
+        replace = ",\n\t\tBasicLevel('{0}', pkg_resources.resource_filename(__name__, '_{0}/'))\n\t]".format(level_name)
+        filedata = filedata.replace("\n\t]", replace)
+        filedata = filedata.replace("[,","[")
 
         with open(join(skill_path,"__init__.py"), 'w') as fp:
             fp.write(filedata)
         fp.close()
-
-    # Add skill to category to skills/__init__.py
-    with open(join(join("gitgud","skills"),"__init__.py"), 'r') as fp:
-        filedata = fp.read()
-    fp.close()
-        
-    replace = ",\n\t{}_skill\n]".format(skill_name)
-    filedata = filedata.replace("\n]", replace)
-        
-    with open(join(join("gitgud","skills"),"__init__.py"), 'w') as fp:
-        fp.write(filedata)
-    fp.close()
-
-    print(fp.closed) # for debug, delete
+        print(fp.closed) #for debug, delete
     return
 
 def main():
