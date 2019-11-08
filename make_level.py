a4323453da4174147d7f722f684d58ea26d2e273
Michael Park <parkmichael777@gmail.com>
2019-11-08 12:24:19 -0600

    Can write to skills/init + broke writing to skills/skill_name/init

--- make_level.py
+++ make_level.py
@@ -2,31 +2,46 @@ import sys
 import os
 
 path = os.path
+join = os.path.join
 cwd = os.getcwd()
 
-def writeToLine(skill_name, skill_path, level_name):
-    if not path.exists(path.join(skill_path,"__init__.py")):
-        with open(path.join(skill_path,"__init__.py"), 'w+') as fp:
+def writeInit(skill_name, skill_path, level_name):
+    # If skills/skill/__init__.py doesn't exist, create a basic version (no levels)
+    if not path.exists(join(skill_path,"__init__.py")):
+        with open(join(skill_path,"__init__.py"), 'w+') as fp:
             level_setup = "import pkg_resources\nfrom gitgud.skills.util import BasicLevel\nfrom gitgud.skills.util import Skill\n\nskill = Skill(\n\t'{}',\n\t[\n\t]\n)".format(skill_name)
             fp.write(level_setup)
-            print(fp.closed) #for debug, delete
         fp.close()
-        writeToLine(skill_name, skill_path, level_name)
+        writeInit(skill_name, skill_path, level_name)
+        print(fp.closed) #for debug, delete
+        return
     else:
-        with open(path.join(skill_path,"__init__.py"), 'r') as fp:
+        # Populate file with BasicLevel
+        with open(join(skill_path,"__init__.py"), 'r') as fp:
             filedata = fp.read()
         fp.close()
 
-        replace = "[\n\t\tBasicLevel('{0}', pkg_resources.resource_filename(__name__, '_{0}/')),".format(level_name)
-        filedata = filedata.replace("[", replace)
-        filedata = filedata.replace(",\n\t]","\n\t]")
+        replace = "\b\b,\n\t\tBasicLevel('{0}', pkg_resources.resource_filename(__name__, '_{0}/'))\n\t\t]".format(level_name)
+        filedata = filedata.replace("]", replace)
 
-        with open(path.join(skill_path,"__init__.py"), 'w') as fp:
+        with open(join(skill_path,"__init__.py"), 'w') as fp:
             fp.write(filedata)
         fp.close()
-            
-    
+
+    # Add skill to category to skills/__init__.py
+    with open(join(join("gitgud","skills"),"__init__.py"), 'r') as fp:
+        filedata = fp.read()
+    fp.close()
+        
+    replace = ",\n\t{}_skill\n]".format(skill_name)
+    filedata = filedata.replace("\n]", replace)
+        
+    with open(join(join("gitgud","skills"),"__init__.py"), 'w') as fp:
+        fp.write(filedata)
+    fp.close()
+
     print(fp.closed) # for debug, delete
+    return
 
 def main():
     # Obtain input arguments
@@ -42,34 +57,34 @@ def main():
                 return
             
             # Make skill folder
-            skill_path = path.join("gitgud","skills","{}".format(skill_name))
+            skill_path = join("gitgud","skills","{}".format(skill_name))
             if not path.exists(skill_path):
                 os.mkdir(skill_path)
     
             # Make level folder
-            level_path = path.join(skill_path,"_{}".format(level_name))
+            level_path = join(skill_path,"_{}".format(level_name))
             if not path.exists(level_path):
                 os.mkdir(level_path)
             
-            writeToLine(skill_name,skill_path,level_name)
+            writeInit(skill_name,skill_path,level_name)
             
             # Make instruction file
-            inst_path = path.join(level_path,"instructions.txt")
+            inst_path = join(level_path,"instructions.txt")
             if not path.exists(inst_path):
                 open(inst_path,'a').close()
             
             # Make goal file
-            goal_path = path.join(level_path,"goal.txt")
+            goal_path = join(level_path,"goal.txt")
             if not path.exists(goal_path):
                 open(goal_path,'a').close()
             
             # Make setup file
-            setup_path = path.join(level_path,"setup.spec")
+            setup_path = join(level_path,"setup.spec")
             if not path.exists(setup_path):
                 open(setup_path,'a').close()
     
             # Make test file
-            test_path = path.join(level_path,"test.spec")
+            test_path = join(level_path,"test.spec")
             if not path.exists(test_path):
                 open(test_path,'a').close()
     else:
