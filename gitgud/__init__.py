754f1bb98e68d145996e1a2446d6cf3755deaa82
Ben Thayer <ben@benthayer.com>
2019-11-08 20:44:58 -0600

    CircleCI

--- gitgud/__init__.py
+++ gitgud/__init__.py
@@ -1,35 +1,15 @@
 import sys
 import os
-
-from configparser import NoSectionError
+import subprocess
 
 from git import Actor
-from git import Repo
 
 actor = Actor("Git Gud", "git-gud@example.com")
 actor_string = "Git Gud <git-gud@example.com>"
 
 
-# TODO GitPython global config parser without repo
-class GitConfig(Repo):
-    def __init__(self, *args, **kwargs):
-        #
-        # Work around the GitPython issue #775
-        # https://github.com/gitpython-developers/GitPython/issues/775
-        #
-        self.git_dir = os.path.join(os.getcwd(), ".git")
-        Repo.__init__(self, *args, **kwargs)
-
-
-def create_alias(config_writer=None):
+def create_alias():
     python = sys.executable.replace('\\', '/')  # Git uses unix-like path separators
 
-    if config_writer is None:
-        config_writer = GitConfig().config_writer(config_level='global')
-
-    try:
-        config_writer.remove_option('alias', 'gud')
-    except NoSectionError:
-        pass
-    config_writer.add_value('alias', 'gud', '"! {} -m gitgud"'.format(python))
+    subprocess.call(['git', 'config', '--global', 'alias.gud', '! "{}" -m gitgud'.format(python)])
 
