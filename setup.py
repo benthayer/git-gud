830d22216bfb7dd812792d08db2fe58202ce7cb8
Michael Park <parkmichael777@gmail.com>
2019-11-17 12:11:24 -0600

    Resolving merge conflict2

--- setup.py
+++ setup.py
@@ -33,21 +33,21 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=[
-            'gitgud',
-            'gitgud.hooks',
-            'gitgud.skills',
-            'gitgud.skills.basics',
-            'gitgud.skills.extras',
-            'gitgud.skills.rampup',
-            'git-gud.make_level.py',
-            'git-gud.level_file_temp'
+        'gitgud',
+        'gitgud.hooks',
+        'gitgud.skills',
+        'gitgud.skills.basics',
+        'gitgud.skills.extras',
+        'gitgud.skills.rampup',
+        'git-gud.make_level.py',
+        'git-gud.level_file_temp'
     ],
     package_data={
-            'gitgud.skills.basics': ['_*/*'],
-            'gitgud.skills.extras': ['_*/*'],
-            'gitgud.skills.rampup': ['_*/*'],
+        'gitgud.skills.basics': ['_*/*'],
+        'gitgud.skills.extras': ['_*/*'],
+        'gitgud.skills.rampup': ['_*/*'],
     },
-    python_requires='>=3.0',
+    python_requires='>=3.5',
     install_requires=[
         'gitpython',
     ],
