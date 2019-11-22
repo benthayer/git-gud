89e3e4b00a223c452cd67a5cd32efa2b55090ab0
Sahan Kumarasinghe <sahansk2@illinois.edu>
2019-11-22 14:52:36 -0600

    Modified tests to not use ints for gets

--- gitgud/test_commands.py
+++ gitgud/test_commands.py
@@ -6,13 +6,13 @@ from gitgud.skills import all_skills
 
 def test_load(gg):
     load_tests = [
-        ('git gud load 1', all_skills[0][0]),
-        ('git gud load rampup', all_skills["rampup"][0]),
-        ('git gud load 2 detaching', all_skills[1]["detaching"]),
-        ('git gud load rampup 4', all_skills["rampup"][3]),
-        ('git gud load 3-octopus', all_skills[2]["octopus"]),
-        ('git gud load basics-4', all_skills["basics"][3]),
-        ('git gud load -2', all_skills["basics"][1])
+        ('git gud load 1', all_skills["1"]["1"]),
+        ('git gud load rampup', all_skills["rampup"]["1"]),
+        ('git gud load 2 detaching', all_skills["2"]["detaching"]),
+        ('git gud load rampup 4', all_skills["rampup"]["4"]),
+        ('git gud load 3-octopus', all_skills["3"]["octopus"]),
+        ('git gud load basics-4', all_skills["basics"]["4"]),
+        ('git gud load -2', all_skills["basics"]["2"])
     ]
     
     for command, level in load_tests:
