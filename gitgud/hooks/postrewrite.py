2af3bfa9459ac7bc4d5e5f0139c609abe5a02827
Michael Park <parkmichael777@gmail.com>
2019-12-30 16:56:14 -0600

    Logging is prettier

--- gitgud/hooks/postrewrite.py
+++ gitgud/hooks/postrewrite.py
@@ -2,10 +2,15 @@ import sys
 
 # TODO Git hooks should not actually do any logging
 # TODO Keep track of rebased commits
-print(sys.argv[1])
+
+print()
+if "rebase" in sys.argv:
+    print("Logging for Rebase:")
+elif "amend" in sys.argv:
+    print("Logging for Amended Commit:")
 
 for i, line in enumerate(sys.stdin):
     old_hash, new_hash = line.split()
-    print('Change {}'.format(i + 1))
-    print('Old hash: ', old_hash)
-    print('New hash: ', new_hash)
+    print('Change #{}:'.format(i + 1))
+    print('    Old hash: ', old_hash)
+    print('    New hash: ', new_hash)
