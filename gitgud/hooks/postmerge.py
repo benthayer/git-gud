fb7325da42554299c204f42af798640f05639e97
Michael Park <parkmichael777@gmail.com>
2019-12-30 16:56:36 -0600

    Initial setup for track merges

---
+++ gitgud/hooks/postmerge.py
@@ -0,0 +1,3 @@
+# TODO: Currently, merges are denoted as M# in setup.spec, which causes CM# in commits tracking, may need to fix that here.
+# TODO: Check where init method of commits and merges json needs to be placed.
+
