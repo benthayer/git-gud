b8837446edb701a2a49ff137da1b61bb9ac0ba8b
Michael Park <parkmichael777@gmail.com>
2020-01-08 22:22:24 -0600

    Renames rebased commits and tracks rebases

--- gitgud/hooks/postrewrite.py
+++ gitgud/hooks/postrewrite.py
@@ -1,21 +1,61 @@
 import sys
-import os
+import json
 import subprocess
 
-# TODO Git hooks should not actually do any logging
-# TODO Keep track of rebased commits
-subprocess.call(["GIT_EDITOR=" + os.path.join(file_operator.hooks_path, "rebasehandler.sh"), "git", "rebase", "-i", "--no-verify",  commit.message.strip() + "'"])
+from gitgud.operations import get_operator
 
-"""
+
+file_operator = get_operator()
+with open(file_operator.rebase_info_path) as fp:
+    rebase_info = json.load(fp)
+
+# Create Tree
+tree_dict = {}
+testing_commits = set()     # Commits being processed.
+tree_commits = set()        # Holds all the commit objects in the tree.
+
+# First, add all branch commits.
+for branch in file_operator.repo.branches:
+    testing_commits.add(branch.commit)
+
+# Second, add detached head commits.
+# Branch commits + (possible) Detached HEAD commit = All possible leaf nodes.
+if file_operator.repo.head.is_detached:
+    testing_commits.add(file_operator.repo.head.commit)
+
+# Add all internal nodes to tree_commits set.
+while len(testing_commits) > 0:
+    test_commit = testing_commits.pop()
+    for parent in test_commit.parents:
+        if parent not in tree_commits:
+            testing_commits.add(parent)
+    tree_commits.add(test_commit)
+    tree_dict[test_commit.message.strip()] = str(test_commit)
+
+# TODO: Git hooks should not actually do any logging
+# TODO: Keep track of rebased commits
+
+with open(file_operator.commits_json_path) as fp:
+    commits_json = json.load(fp)
+
 print()
 if "rebase" in sys.argv:
     print("Logging for Rebase:")
+    for key in rebase_info:
+        if key != 'rebased' and key != 'rebase_to':
+            print("Changes for Commit: " + key)
+            print("    Old Hash: " + rebase_info[key])
+            print("    New Hash: " + tree_dict[key + "'"])
+            # Does not delete old commit info.
+            commits_json[key + "'"] = [rebase_info[key][:7], tree_dict[key + "'"][:7]]
+    print()
 elif "amend" in sys.argv:
     print("Logging for Amended Commit:")
+    for i, line in enumerate(sys.stdin):
+        old_hash, new_hash = line.split()
+        print('Change #{}:'.format(i + 1))
+        print('    Old hash: ', old_hash)
+        print('    New hash: ', new_hash)
 
-for i, line in enumerate(sys.stdin):
-    old_hash, new_hash = line.split()
-    print('Change #{}:'.format(i + 1))
-    print('    Old hash: ', old_hash)
-    print('    New hash: ', new_hash)
-"""
+with open(file_operator.commits_json_path, 'w') as fp:
+    json.dump(commits_json, fp)
--- gitgud/hooks/prerebase.py
+++ gitgud/hooks/prerebase.py
@@ -1,70 +1,101 @@
 import sys
 import json
-
-from gitgud.operations import file_operator
-
-file_operator = get_operator()
-with open(file_operator.gg_path
-
-import os
-import sys
 import subprocess
 
 from gitgud.operations import get_operator
 
 file_operator = get_operator()
-rebase_to_branch = sys.argv[1]
+rebase_to_arg = sys.argv[1]     # Name/hash of the destination branch/commit
 
-"""
-if file_operator.repo.head.is_detached:
-    head_name = file_operator.repo.commit('HEAD').message.strip()
-    print(head_name)
-else:
-    head_name = file_operator.repo.head.ref.name
-    print(head_name)
-    for branch in file_operator.repo.branches:
-        if (branch == head_name):
-            commits.add(branch.commit)
-            commit_name = branch.commit.message.strip()
-            print(commits)
-    print(head_name)"""
-    
+# Need the commit belonging to the HEAD (rebase reference)
+# And need the commit belonging to the destination.
+head_commit = file_operator.repo.head.commit
+rebase_to_commit = ""           # Placeholder Instantiation
 
-rebasing_commits = set()        # Commits referenced by rebased branch.
-rebasing_const = set()          # Set used to construct rebasing_commits.
-rebase_to_commits = set()       # Commits referenced by destination branch.
-rebase_to_const = set()         # Set used to construct rebasing_to_commits.
-rebased_commits = set()         # Commits that were rebased.
-
-# TODO: Add detached head option.
-head_name = file_operator.repo.head.ref.name
+# If destination is a branch name, finding the rebase_to commit is easy:
 for branch in file_operator.repo.branches:
-    if (str(branch) == head_name):
-        rebasing_const.add(branch.commit)
-        while len(rebasing_const) > 0:
-            testing = rebasing_const.pop()
-            for parent in testing.parents:
-                if parent not in rebasing_const:
-                    rebasing_const.add(parent)
-            rebasing_commits.add(testing)
-    elif (str(branch) == rebase_to_branch):
-        rebase_to_const.add(branch.commit)
-        while len(rebase_to_const) > 0:
-            testing = rebase_to_const.pop()
-            for parent in testing.parents:
-                if parent not in rebase_to_const:
-                    rebase_to_const.add(parent)
-            rebase_to_commits.add(testing)
+    if str(branch) == rebase_to_arg:
+        rebase_to_commit = branch.commit
 
-while len(rebasing_commits) > 0:
-    cur_commit = rebasing_commits.pop()
-    if cur_commit not in rebase_to_commits:
-        rebased_commits.add(cur_commit)
+# If destination is a commit hash,
+# need to compare against all commit hashes to find object.
 
-amender = file_operator.repo.create_head('amender')
-for commit in rebased_commits:
-    amender.commit = commit
-    subprocess.call(["GIT_EDITOR=" + os.path.join(file_operator.hooks_path, "rebasehandler.sh"), "git", "rebase", "-i", "--no-verify",  commit.message.strip() + "'"])
+testing_commits = set()     # Commits being processed.
+tree_commits = set()        # Holds all the commit objects in the tree.
+
+# First, add all branch commits.
+for branch in file_operator.repo.branches:
+    testing_commits.add(branch.commit)
     
-file_operator.repo.delete_head(amender)
+# Second, add detached head commit.
+# Branch commits + (possible) Detached HEAD commit = All possible leaf nodes.
+if file_operator.repo.head.is_detached:
+    testing_commits.add(file_operator.repo.head.commit)
+    
+# Add all internal nodes to tree_commits set.
+while len(testing_commits) > 0:
+    test_commit = testing_commits.pop()
+    for parent in test_commit.parents:
+        if parent not in tree_commits:
+            testing_commits.add(parent)
+    tree_commits.add(test_commit)
+
+# Search tree for matching destination hash.
+if rebase_to_commit == "":
+    for commit in tree_commits:
+        if rebase_to_arg in str(commit)[:(len(rebase_to_arg) - 1)]:
+            rebase_to_commit = commit
+            break
+
+# Now, use these two commits to find the set of rebased commit objects.
+rebasing_const = set()          # Set used to construct rebasing_commits.
+rebasing_commits = set()        # Commits referenced by rebased branch.
+rebase_to_const = set()         # Set used to construct rebasing_to_commits.
+rebase_to_commits = set()       # Commits referenced by destination branch.
+
+# Record all commits in the rebase_to branch.
+rebase_to_const.add(rebase_to_commit)
+while len(rebase_to_const) > 0:
+    testing = rebase_to_const.pop()
+    for parent in testing.parents:
+        if parent not in rebase_to_commits:
+            rebase_to_const.add(parent)
+    rebase_to_commits.add(testing)
+
+# Record all commits that were rebased.
+rebasing_const.add(head_commit)
+while len(rebasing_const) > 0:
+    testing = rebasing_const.pop()
+    for parent in testing.parents:
+        if parent not in rebasing_commits and parent not in rebase_to_commits:
+            rebasing_const.add(parent)
+    rebasing_commits.add(testing)
+
+# Store cmd line name of rebase ref and destination ref.
+rebase_info = {}
+if file_operator.repo.head.is_detached:
+    # Detached Commit Hash
+    rebase_info['rebased'] = str(file_operator.repo.head.commit)
+elif file_operator.repo.head.ref.name in file_operator.repo.branches:
+    # Branch Name
+    rebase_info['rebased'] = file_operator.repo.head.ref.name
+rebase_info['rebase_to'] = rebase_to_arg    # Can be hash or branch name.
+
+# For each commit that is rebased:
+#   - Store {(message:hash)}
+for commit in rebasing_commits:
+    rebase_info[commit.message.strip()] = str(commit)
+
+# Store rebase_info into json file for use by postrewrite.py.
+with open(file_operator.rebase_info_path, 'w') as fp:
+    json.dump(rebase_info, fp)
+
+# Upon exiting this python script, the shell script will abort the user's rebase.
+# It is essentially being replaced by ours below.
+# TODO: Parse user-selected options.
+print("Cancelling user rebase and replacing with simulation.")
+print("Simulating 'git rebase':")
+cmd = 'git commit --amend --no-post-rewrite --quiet -m"$(git log -n1 --format=%s)\'"'
+subprocess.call(["git", "rebase", rebase_to_arg, "--no-verify", "-x", cmd])
+
 
