28484a26314ffc76edae8101aa6ab3d6bd2d1331
Ben Thayer <ben@benthayer.com>
2020-03-09 01:47:17 -0500

    Updated NamedList to start at specified index

--- gitgud/skills/util.py
+++ gitgud/skills/util.py
@@ -1,20 +1,13 @@
 class NamedList:
     # names is a list populated with type str, items is a list populated with any type 
-    def __init__(self, names, items):
+    def __init__(self, names, items, start_index=1):
         assert len(names) == len(items)
         self._name_dict = {name: index for index, name in enumerate(names)}
+        self._name_dict.update({str(index + start_index): index for index in range(len(names))})
         self._items = items
     
     def __getitem__(self, query):
-        if isinstance(query, str):
-            if query.isnumeric():
-                if 0 < int(query) <= len(self):
-                    return self._items[int(query) - 1]
-                else:
-                    raise KeyError
-            return self._items[self._name_dict[query]]
-        else:
-            raise KeyError
+        return  self._items[self._name_dict[query]]
 
     def __iter__(self):
         return self._items.__iter__()
@@ -36,7 +29,7 @@ class NamedList:
         return self._items
     
     def keys(self):
-        set_indices = { str(i) for i in range(1, len(self) + 1) }
+        set_indices = { str(i) for i in range(len(self)) }
         set_names = set(self._name_dict.keys())
         return set_indices | set_names
         
@@ -44,7 +37,7 @@ class NamedList:
 
 class AllSkills(NamedList):
     def __init__(self, skills):
-        super().__init__([skill.name for skill in skills], skills)
+        super().__init__([skill.name for skill in skills], skills, start_index=0)
         last_level = None
         for skill in self:
             for level in skill:
--- gitgud/test_commands.py
+++ gitgud/test_commands.py
@@ -8,9 +8,9 @@ def test_load(gg):
     load_tests = [
         ('git gud load 1', all_skills["1"]["1"]),
         ('git gud load rampup', all_skills["rampup"]["1"]),
-        ('git gud load 3 detaching', all_skills["3"]["detaching"]),
+        ('git gud load 2 detaching', all_skills["2"]["detaching"]),
         ('git gud load rampup 4', all_skills["rampup"]["4"]),
-        ('git gud load 4-octopus', all_skills["4"]["octopus"]),
+        ('git gud load 3-octopus', all_skills["3"]["octopus"]),
         ('git gud load rampup-4', all_skills["rampup"]["4"]),
         ('git gud load -2', all_skills["rampup"]["2"])
     ]
