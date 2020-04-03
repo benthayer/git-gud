3dc4a121aa15afc7738f61affe1d5711ab71279d
Ben Thayer <ben@benthayer.com>
2020-04-03 12:20:30 -0500

    Updated make_level.py to be more readable.

--- make_level.py
+++ make_level.py
@@ -4,6 +4,64 @@ from shutil import copyfile
 
 cwd = os.getcwd()
 
+def register_skill_package(skill_name):
+    # Read setup.py
+    with open("setup.py", 'r') as fp:
+        filedata = fp.read()
+
+    # Register skill if not already registered.
+    if not "gitgud.skills.{}".format(skill_name) in filedata:
+        # Package
+        replace1 = "\n".join([
+            "        \'gitgud.skills.{}\',".format(skill_name),
+            "    ],",
+            "    package_data"
+        ])
+
+        # Data files
+        replace2 = "\n".join([
+            "        \'gitgud.skills.{}\': ['_*/*'],".format(skill_name),
+            "    },",
+            "    python_requires"
+        ])
+
+        # Register new skill to setup.py
+        filedata = filedata.replace("\n".join([
+            "    ],",
+            "    package_data"
+        ]), replace1)
+
+        filedata = filedata.replace("\n".join([
+            "    },",
+            "    python_requires"
+        ]), replace2)
+
+        # Write new lines to file
+        with open("setup.py", 'w') as fp:
+            fp.write(filedata)
+            print("Registered.".format(skill_name))
+            print()
+
+
+def make_folders(skill_name, level_name):
+    # Make skill folder
+    skill_path = os.path.join("gitgud", "skills", "{}".format(skill_name))
+    if not os.path.exists(skill_path):
+        os.mkdir(skill_path)
+        print("Created: {}".format(skill_path))
+    else:
+        print("Exists: {}".format(skill_path))
+
+    # Make level folder
+    level_path = os.path.join(skill_path, "_{}".format(level_name))
+    if not os.path.exists(level_path):
+        os.mkdir(level_path)
+        print("Created: {}".format(level_path))
+    else:
+        print("Exists: {}".format(level_path))
+
+    return skill_path, level_path
+
 
 def write_init(skill_name, skill_path, level_name):
     # If skills/<new_skill>/__init__.py doesn't exist, create a basic version
@@ -98,12 +156,12 @@ def create_level_file(level_path, filename):
     print("Created: {}".format(filepath))
 
 
-def main():
-    # Obtain input arguments
+def get_new_level_name_from_args():
     num_args = len(sys.argv) - 1
     if num_args == 2:
         skill_name = sys.argv[1]
         level_name = sys.argv[2]
+        return skill_name, level_name
     else:
         if num_args > 3:
             error_message = "Too many arguments: "
@@ -111,14 +169,10 @@ def main():
             error_message = "Too few arguments: "
         print(error_message + "Takes 2 arguments, but {} was given.".format(num_args))
         print("Usage: \"python make_level.py <skill_name> <level_name>\"")
-        print()
-        return
-        
-    # Check if current dir isn't ../gitgud directory. (i.e. dir of setup files)
-    if not os.path.isdir(os.path.join(cwd, 'gitgud')):
-        print("Error: Execute this script in the git-gud directory.")
-        return
+        exit(1)
 
+
+def confirm_name(skill_name, level_name):
     # Confirm choice to avoid making a mess
     print("\n".join([
         "skill_name: {}".format(skill_name),
@@ -131,77 +185,45 @@ def main():
         choice = input().lower()
         if choice == 'n':
             print("Aborting, no changes made.")
-            return
+            exit(1)
         elif choice != 'y':
             print("Confirm[y/n] ", end='')
 
-    # Register package
-    # Read setup.py
-    with open("setup.py", 'r') as fp:
-        filedata = fp.read()
 
-    # Register skill if not already registered.
-    if not "gitgud.skills.{}".format(skill_name) in filedata:
-        # Concatenate potential entry
-        replace1 = "\n".join([
-            "        \'gitgud.skills.{}\',".format(skill_name),
-            "    ],",
-            "    package_data"
-        ])
-
-        replace2 = "\n".join([
-            "        \'gitgud.skills.{}\': ['_*/*'],".format(skill_name),
-            "    },",
-            "    python_requires"
-        ])
-
-        # Register new skill to setup.py
-        filedata = filedata.replace("\n".join([
-            "    ],",
-            "    package_data"
-        ]), replace1)
-
-        filedata = filedata.replace("\n".join([
-            "    },",
-            "    python_requires"
-        ]), replace2)
-
-        # Write new lines to file
-        with open("setup.py", 'w') as fp:
-            fp.write(filedata)
-            print()
-            print("Registered Package: {}".format(skill_name))
-            print()
-
-    print("Creating Folders:")
-    # Make skill folder
-    skill_path = os.path.join("gitgud", "skills", "{}".format(skill_name))
-    if not os.path.exists(skill_path):
-        os.mkdir(skill_path)
-        print("Created: {}".format(skill_path))
-
-    # Make level folder
-    level_path = os.path.join(skill_path, "_{}".format(level_name))
-    if not os.path.exists(level_path):
-        os.mkdir(level_path)
-        print("Created: {}".format(level_path))
-
+def main():
+    skill_name, level_name = get_new_level_name_from_args()
+        
+    # Check if current dir isn't ../gitgud directory. (i.e. dir of setup files)
+    if not os.path.isdir(os.path.join(cwd, 'gitgud')):
+        print("Error: Script must be run in the git-gud directory.")
+        exit(1)
+    
+    confirm_name(skill_name, level_name)
     print()
+    
+    print("Registering package: {}".format(skill_name))
+    register_skill_package(skill_name)
+    print()
+    
+    print("Creating Folders:")
+    skill_path, level_path = make_folders(skill_name, level_name)
+    print()
+
     print("Registering Level: {} {}".format(skill_name, level_name))
     write_init(skill_name, skill_path, level_name)
-
     print()
+
     print("Creating Test Case:")
     write_test(skill_path, level_name)
-
     print()
+
     print("Creating Files:")
     create_level_file(level_path, "instructions.txt")
     create_level_file(level_path, "goal.txt")
     create_level_file(level_path, "setup.spec")
     create_level_file(level_path, "test.spec")
-
     print()
+
     print("Done.")
 
 
