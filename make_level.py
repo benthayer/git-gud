17cd4d5d32558ffbeba4685fc427aae7214f95c8
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-07-08 17:10:03 -0400

    Update make_level.py to be consistent/proper.

--- make_level.py
+++ make_level.py
@@ -42,7 +42,7 @@ def register_skill_package(skill_name):
         print('Registered package "{}" in setup.py'.format(skill_name))
 
 
-def make_folders(skill_name, level_name):
+def make_folders(level_name, skill_name):
     # Make skill folder
     skill_path = os.path.join("gitgud", "skills", "{}".format(skill_name))
     if not os.path.exists(skill_path):
@@ -59,7 +59,7 @@ def make_folders(skill_name, level_name):
     else:
         print("Exists: {}".format(level_path))
 
-    return skill_path, level_path
+    return level_path, skill_path
 
 
 def make_skill(skill_name, skill_long_name, skill_path):
@@ -97,7 +97,7 @@ def make_skill(skill_name, skill_long_name, skill_path):
     print("Registered skill \"{}\" in {}".format(skill_name, filepath))
 
 
-def make_level(skill_name, skill_path, level_name, level_long_name):
+def make_level(level_name, level_long_name, skill_name, skill_path):
     # Add level to skills/<new_skill>/__init__.py
     filepath = os.path.join(skill_path, "__init__.py")
     with open(filepath, 'r') as fp:
@@ -117,7 +117,7 @@ def make_level(skill_name, skill_path, level_name, level_long_name):
     print("Registered level \"{}\" in {}".format(level_name, filepath))
 
 
-def write_test(skill_path, skill_name):
+def write_test(skill_path):
     test_levels_path = os.path.join(skill_path, "test_levels.py")
     if not os.path.exists(test_levels_path):
         copyfile("level_file_templates/test_levels.py", test_levels_path)
@@ -134,23 +134,23 @@ def create_level_file(level_path, filename):
 
 def get_new_level_name_from_args():
     num_args = len(sys.argv) - 1
-    if num_args == 4:
-        skill_name = sys.argv[1]
-        skill_long_name = sys.argv[2]
-        level_name = sys.argv[3]
-        level_long_name = sys.argv[4]
-        return skill_name, skill_long_name, level_name, level_long_name
+    if num_args == 3 or num_args == 4:
+        level_name = sys.argv[1]
+        level_long_name = sys.argv[2]
+        skill_name = sys.argv[3]
+        skill_long_name = sys.argv[4] if num_args == 4 else ""
+        return level_name, level_long_name, skill_name, skill_long_name
     else:
         if num_args > 4:
             error_message = "Too many arguments: "
         else:
             error_message = "Too few arguments: "
-        print(error_message + "Takes 2 arguments, but {} was given.".format(num_args))  # noqa: E501
-        print('Usage: "python make_level.py <skill_name> <skill_long_name> <level_name> <level_long_name>"')  # noqa: E501
+        print(error_message + "Takes 3 or 4 arguments, but {} was given.".format(num_args))  # noqa: E501
+        print('Usage: "python make_level.py <level_name> <level_long_name> <skill_name> <skill_long_name>"')  # noqa: E501
         exit(1)
 
 
-def confirm_name(skill_name, level_name):
+def confirm_name(level_name, skill_name):
     # Confirm choice to avoid making a mess
     print("\n".join([
         "skill_name: {}".format(skill_name),
@@ -169,14 +169,14 @@ def confirm_name(skill_name, level_name):
 
 
 def main():
-    skill_name, skill_long_name, level_name, level_long_name = get_new_level_name_from_args()  # noqa: E501
+    level_name, level_long_name, skill_name, skill_long_name = get_new_level_name_from_args()  # noqa: E501
 
     # Check if current dir isn't ../gitgud directory. (i.e. dir of setup files)
     if not os.path.isdir(os.path.join(os.getcwd(), 'gitgud')):
         print("Error: Script must be run in the git-gud directory.")
         exit(1)
 
-    confirm_name(skill_name, level_name)
+    confirm_name(level_name, skill_name)
     print()
 
     print("Registering package: {}".format(skill_name))
@@ -184,20 +184,26 @@ def main():
     print()
 
     print("Creating Folders:")
-    skill_path, level_path = make_folders(skill_name, level_name)
+    level_path, skill_path = make_folders(level_name, skill_name)
     print()
 
     if not os.path.exists(os.path.join(skill_path, "__init__.py")):
+        if not skill_long_name:
+            print("Error: Skill's long name must be specified if skill doesn't already exist.")  # noqa: E501
+            exit(1)
         print("Registering Skill:")
         make_skill(skill_name, skill_long_name, skill_path)
         print()
 
+    if not level_long_name:
+        print("Error: Level's long name must be specified in order to register a level.")  # noqa: E501
+        exit(1)
     print("Registering Level:")
-    make_level(skill_name, skill_path, level_name, level_long_name)
+    make_level(level_name, level_long_name, skill_name, skill_path)
     print()
 
     print("Creating Test Case:")
-    write_test(skill_path, level_name)
+    write_test(skill_path)
     print()
 
     print("Creating Files:")
