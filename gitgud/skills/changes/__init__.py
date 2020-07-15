bb6dea031a74d7832527a0f36cbf206f8067a959
Ben Thayer <ben@benthayer.com>
2020-07-15 15:15:52 -0500

    Setup first commit level

--- gitgud/skills/changes/__init__.py
+++ gitgud/skills/changes/__init__.py
@@ -1,14 +1,19 @@
 from gitgud.skills.level_builder import BasicLevel
 from gitgud.skills.util import Skill
+from gitgud.operations import get_operator
 
 class FirstCommit(BasicLevel):
+    def _setup(self):
+        get_operator().repo.git.checkout('--orphan', 'master')
+
     def post_setup(self):
-        pass
+        self.display_message('post-setup.txt')
 
     def _test(self):
         # As long as there's one commit, it doesn't matter what's in it
         # Use the existing setup so that any commit matches the first one
-        # Test branches 
+        # Test branches
+        pass
 
 
 
