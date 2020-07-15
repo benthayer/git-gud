2ea935fd819399370d7b5905eea68b56a0bfc5e4
Ben Thayer <ben@benthayer.com>
2020-07-15 15:54:56 -0500

    Updated test_commands to resolve some issues

--- gitgud/test_commands.py
+++ gitgud/test_commands.py
@@ -10,11 +10,11 @@ def test_load(gg):
         ('git gud load rampup', all_skills["rampup"]["1"]),
         ('git gud load 3 detaching', all_skills["3"]["detaching"]),
         ('git gud load rampup 4', all_skills["rampup"]["4"]),
-        ('git gud load 5-octopus', all_skills["5"]["octopus"]),
+        ('git gud load 6-octopus', all_skills["6"]["octopus"]),
         ('git gud load rampup-4', all_skills["rampup"]["4"]),
         ('git gud load -2', all_skills["rampup"]["2"])
     ]
 
     for command, level in load_tests:
         subprocess.call(command, shell=True)
-        assert level == gg.get_level()
+        assert gg.get_level() == level
