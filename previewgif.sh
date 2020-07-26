673fe7198ddef92a3e86ca137533048765c367b0
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-07-26 13:14:03 -0400

    Added concept file for asciinema preview

---
+++ previewgif.sh
@@ -0,0 +1,53 @@
+# Required Environments
+# DEMOMAGICPATH="$(realpath demomagic.sh)"
+
+# ===== Configure demo-magic.sh
+. $DEMO_MAGIC_PATH
+PROMPT_TIMEOUT=1
+TYPE_SPEED=15
+DEMO_PROMPT="${GREEN}➜ ${CYAN}\W${RED}\$ "
+
+# ===== Configure Shell 
+OLD_HOME=$(pwd)
+ 
+# We want to exit if any command doesn't work, or if
+# an undefined variable is used.
+set -euo pipefail
+
+cd /tmp
+clear
+
+# ===== Begin presentation 
+p "pip3 install git-gud"
+pip3 install ${GITGUD_REPO}
+
+p "We'll first make an empty directory for git gud."
+pe "mkdir my-git-gud"
+pe "cd my-git-gud"
+pe "git gud init"
+wait
+pe "git gud load 0 3"
+pe "git gud status"
+pe "git init"
+pe "git gud status"
+pe "git gud test"
+pe "git gud load 1 1"
+
+pe "git gud commit"
+pe "git gud commit"
+pe "git gud show tree"
+
+pe "git gud test"
+pe "git gud load next"
+pe "git branch bugFix"
+pe "git checkout bugFix"
+pe "git gud commit"
+pe "git gud test"
+pe "git gud levels --all"
+pe "git gud load 3-octopus"
+pe "git merge feature1 feature2"
+p ""
+
+# ===== Cleanup
+rm -rf /tmp/my-git-gud
+cd $OLD_HOME
