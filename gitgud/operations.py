7ff0bf6057be36917ec0a92222a2b152cfd71901
Ben Thayer <ben@benthayer.com>
2020-09-13 14:49:35 -0500

    Fix errors setting up levels during rebases and bisects

--- gitgud/operations.py
+++ gitgud/operations.py
@@ -6,6 +6,7 @@ import email.utils
 from pathlib import Path
 
 from git import Repo, Git
+from git.exc import GitCommandError
 from git.exc import InvalidGitRepositoryError
 
 from gitgud import actor
@@ -151,9 +152,16 @@ class Operator():
                 skip_hooks=True)
         return commit_obj
 
-    def create_tree(self, commits, head, details, level_dir):
-        if not details:
-            details = {}
+    def normalize_state(self):
+        # Make sure we're in a normal state
+        try:
+            self.repo.git.rebase('--abort')
+        except GitCommandError:
+            pass
+        try:
+            self.repo.git.bisect('reset')
+        except GitCommandError:
+            pass
 
         self.clear_tree_and_index()
 
@@ -167,12 +175,23 @@ class Operator():
         # Detach HEAD so we can delete branches
         self.repo.git.checkout(self.repo.head.commit)
 
+    def reset_repo(self):
+        self.normalize_state()
+
         branches = self.repo.branches
         for branch in branches:
             self.repo.delete_head(branch, force=True)
+
         self.repo.delete_tag(*self.repo.tags)
+
         for remote in self.repo.remotes:
             self.repo.delete_remote(remote)
+
+    def create_tree(self, commits, head, details, level_dir):
+        if not details:
+            details = {}
+
+        self.reset_repo()
 
         commit_objects = {}
         counter = len(commits)
---
+++ gitgud/test_operations.py
@@ -0,0 +1,56 @@
+from git.exc import GitCommandError
+
+from gitgud.skills import all_skills
+from gitgud.operations import get_operator
+
+
+def setup_rebase_conflict():
+    file_operator = get_operator()
+
+    with open("foo", "w") as f:
+        f.write("branch 1")
+    file_operator.repo.index.add("foo")
+    file_operator.repo.index.commit("Add a file")
+
+    file_operator.repo.git.checkout('-b', 'branch', 'HEAD~')
+    with open("foo", "w") as f:
+        f.write("branch 2")
+    file_operator.repo.index.add("foo")
+    file_operator.repo.index.commit("Add a file")
+
+    try:
+        file_operator.repo.git.rebase('master')
+    except GitCommandError:
+        # This will happen every time
+        pass
+
+
+def setup_bisect():
+    file_operator = get_operator()
+    file_operator.repo.git.bisect('start')
+
+
+def test_reset_during_rebase_conflict(gg):
+    all_skills["1"]["1"]._setup()
+    setup_rebase_conflict()
+    all_skills["1"]["1"]._setup()
+
+
+def test_reset_during_bisect(gg):
+    all_skills["1"]["1"]._setup()
+    setup_bisect()
+    all_skills["1"]["1"]._setup()
+
+
+def test_reset_during_bisect_then_rebase_conflict(gg):
+    all_skills["1"]["1"]._setup()
+    setup_bisect()
+    setup_rebase_conflict()
+    all_skills["1"]["1"]._setup()
+
+
+def test_reset_during_rebase_conflict_then_bisect(gg):
+    all_skills["1"]["1"]._setup()
+    setup_rebase_conflict()
+    setup_bisect()
+    all_skills["1"]["1"]._setup()
